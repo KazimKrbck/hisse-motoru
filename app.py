7cd186a73ea1fa19241366c7271586b513b16b8a
KazimKrbck <graskii@gmail.com>
2026-04-12 14:18:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if uploaded_file is not None and gemini_api_key:
         with st.spinner("Gemini resmi inceliyor, hisseleri buluyor..."):
             try:
                 genai.configure(api_key=gemini_api_key)
-                model = genai.GenerativeModel('gemini-1.5-flash')
+                model = genai.GenerativeModel('gemini-1.5-flash-latest')
                 img = Image.open(uploaded_file)
                 
                 prompt = """
